640028c4caafcbae9a5eb43630375a26547c2516
wangyonglin <admin@wangyonglin.com>
2021-07-20 03:36:31 -0400

    OK

--- main/app_mqtt.c
+++ main/app_mqtt.c
@@ -16,19 +16,20 @@ static const char *TAG = "MQTT";
 #define WANGYONGLIN_MQTT_BROKER_URL "mqtt://broker.wangyonglin.com"
 #define WANGYONGLIN_MQTT_USERNAME "wangyonglin"
 #define WANGYONGLIN_MQTT_PASSWORD "W@ng0811"
-#define WANGYONGLIN_MQTT_TOPIC "jscs"
+#define WANGYONGLIN_MQTT_TOPIC "/wangyonglin/relay/set"
 objMessage_t message;
-static esp_err_t mqtt_event_handler_cb(esp_mqtt_event_handle_t event)
+
+static void mqtt_event_handler(void *handler_args, esp_event_base_t base, int32_t event_id, void *event_data)
 {
-    objQueue_t *queue = (objQueue_t *)event->user_context;
-    esp_mqtt_client_handle_t client = event->client;
+    ESP_LOGD(TAG, "Event dispatched from event loop base=%s, event_id=%d", base, event_id);
+    esp_mqtt_event_handle_t event = (esp_mqtt_event_handle_t)event_data;
+    objQueue_t *queue = (objQueue_t *)handler_args;
     int msg_id;
-    // your_context_t *context = event->context;
-    switch (event->event_id)
+    switch (event_id)
     {
     case MQTT_EVENT_CONNECTED:
         ESP_LOGI(TAG, "MQTT_EVENT_CONNECTED");
-        msg_id = esp_mqtt_client_subscribe(client, WANGYONGLIN_MQTT_TOPIC, 0);
+        msg_id = esp_mqtt_client_subscribe(event->client, WANGYONGLIN_MQTT_TOPIC, 0);
         ESP_LOGI(TAG, "sent subscribe successful, msg_id=%d", msg_id);
         break;
     case MQTT_EVENT_DISCONNECTED:
@@ -37,7 +38,7 @@ static esp_err_t mqtt_event_handler_cb(esp_mqtt_event_handle_t event)
 
     case MQTT_EVENT_SUBSCRIBED:
         ESP_LOGI(TAG, "MQTT_EVENT_SUBSCRIBED, msg_id=%d", event->msg_id);
-        msg_id = esp_mqtt_client_publish(client, WANGYONGLIN_MQTT_TOPIC, "data", 0, 0, 0);
+        msg_id = esp_mqtt_client_publish(event->client, WANGYONGLIN_MQTT_TOPIC, "data", 0, 0, 0);
         ESP_LOGI(TAG, "sent publish successful, msg_id=%d", msg_id);
         break;
     case MQTT_EVENT_UNSUBSCRIBED:
@@ -47,16 +48,11 @@ static esp_err_t mqtt_event_handler_cb(esp_mqtt_event_handle_t event)
         ESP_LOGI(TAG, "MQTT_EVENT_PUBLISHED, msg_id=%d", event->msg_id);
         break;
     case MQTT_EVENT_DATA:
-        ESP_LOGI(TAG, "MQTT_EVENT_DATA");
         if (objMessage(event->data, &message) == ESP_OK)
         {
-            if (xQueueSend(queue->xQueue, &message, NULL) == pdPASS)
+            if (xQueueSend(queue->xQueue, &message, NULL) != pdPASS)
             {
-                ESP_LOGI(TAG, "xQueueSend OK");
-            }
-            else
-            {
-                ESP_LOGI(TAG, "xQueueSend FAILT");
+                ESP_LOGI(TAG, "xQueueSend Fail");
             }
         }
         break;
@@ -67,25 +63,17 @@ static esp_err_t mqtt_event_handler_cb(esp_mqtt_event_handle_t event)
         ESP_LOGI(TAG, "Other event id:%d", event->event_id);
         break;
     }
-    return ESP_OK;
-}
-
-static void mqtt_event_handler(void *handler_args, esp_event_base_t base, int32_t event_id, void *event_data)
-{
-    ESP_LOGD(TAG, "Event dispatched from event loop base=%s, event_id=%d", base, event_id);
-    mqtt_event_handler_cb(event_data);
 }
 
 void objMqttStart(objQueue_t *queue)
 {
-
     esp_mqtt_client_config_t mqtt_cfg = {
         .client_id = objChipId(),
         .uri = WANGYONGLIN_MQTT_BROKER_URL,
         .username = WANGYONGLIN_MQTT_USERNAME,
-        .password = WANGYONGLIN_MQTT_PASSWORD,
-        .user_context = queue};
+        .password = WANGYONGLIN_MQTT_PASSWORD};
     esp_mqtt_client_handle_t client = esp_mqtt_client_init(&mqtt_cfg);
+
     esp_mqtt_client_register_event(client, ESP_EVENT_ANY_ID, mqtt_event_handler, queue);
     esp_mqtt_client_start(client);
     //esp_mqtt_client_stop(client);
